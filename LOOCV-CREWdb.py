98ef7d0ab331329f6fa9a38e4a8f78876ade105f
Reetika27 <32006688+Reetika27@users.noreply.github.com>
2023-01-16 17:44:00 -0600

    updated-v1
    1. Added functions for Naive Bayes, Deep Neural Network
    Same changes implemented as 5-fold CV updated script

--- LOOCV-CREWdb.py
+++ LOOCV-CREWdb.py
@@ -12,9 +12,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import LeaveOneOut
-from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import OneHotEncoder
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import LabelEncoder
+from collections import Counter
 from sklearn.metrics import mean_squared_error
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold
 from sklearn.metrics import roc_auc_score
 from sklearn.feature_selection import SelectKBest, f_classif
@@ -27,6 +38,7 @@ from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
+
 
 
 # ### Data Pre-Processing
@@ -70,27 +82,16 @@ data.replace(np.nan, '-', inplace = True)
 
 
 X = data.iloc[:, 1:]
-
-
 # In[13]:
-
-
-from sklearn.preprocessing import OneHotEncoder
-
 encoder = OneHotEncoder(handle_unknown='ignore', sparse = False)
 X_encoded = encoder.fit_transform(X)
 X_encoded.shape
+print("X_encoded_shape", X_encoded.shape)
 
 
 # ### Class Rebalancing
 
 # In[14]:
-
-
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import LabelEncoder
-from collections import Counter
-
 y_df = data['REW']
 
 X = X_encoded
@@ -107,186 +108,145 @@ X_smote, y_smote = oversample.fit_resample(X, y_)
 
 
 #Naive Bayes Model
-from sklearn.model_selection import LeaveOneOut
 
-loocv = LeaveOneOut()
+def naiveBayes(X_smote, y_smote, data):
+    loocv = LeaveOneOut()
+    model_cv =GaussianNB()
 
-model_cv =GaussianNB()
+    predicted_prob_dict_cv = dict()
+    auc_s_cv = []
+    accuracy_s_cv = []
+    f1_s_cv = []
+    ytest_cv = []
+    ypred_cv = []
+        
+    #score_id
+    scores_id_cv = []
+    scores_cv = []
+    sample_ids_cv = np.array(data.index)
 
-predicted_prob_dict_cv = dict()
-auc_s_cv = []
-accuracy_s_cv = []
-f1_s_cv = []
-ytest_cv = []
-ypred_cv = []
-    
-#score_id
-scores_id_cv = []
-scores_cv = []
-sample_ids_cv = np.array(data.index)
+    y_pred_proba_cv_list = []
 
-y_pred_proba_cv_list = []
+    for fold, (train_idx, test_idx) in enumerate(loocv.split(X_smote)):   
+        X_train_cv = X_smote[train_idx]
+        X_test_cv = X_smote[test_idx]
 
-for fold, (train_idx, test_idx) in enumerate(loocv.split(X_smote)):   
-  X_train_cv = X_smote[train_idx]
-  X_test_cv = X_smote[test_idx]
+        anova_filter_cv = SelectKBest(f_classif, k= 8)
+        var_filter_cv = VarianceThreshold(threshold= .8 * (1 - .8))
+        anova_pipeline_cv = make_pipeline(var_filter_cv, anova_filter_cv, model_cv)
+        anova_pipeline_cv.fit(X_train_cv, y_smote[train_idx])
+        y_pred_prob_cv = anova_pipeline_cv.predict_proba(X_test_cv)
+        y_pred_cv = anova_pipeline_cv.predict(X_test_cv)
+        
+        
+        #auc_cv = roc_auc_score(y_smote[test_idx], y_pred_prob_cv.flatten(), multi_class='ovr')
+        y_pred_proba_cv_list.append(y_pred_prob_cv.flatten())
+        #auc_s_cv.append(auc_cv)
+        accuracy_s_cv.append(accuracy_score(y_smote[test_idx], y_pred_cv))
+        f1_s_cv.append(f1_score(y_smote[test_idx], y_pred_cv, average="weighted"))
+        scores_id_cv.append(test_idx)
+        scores_cv.append(y_pred_prob_cv)
 
-  anova_filter_cv = SelectKBest(f_classif, k= 8)
-  var_filter_cv = VarianceThreshold(threshold= .8 * (1 - .8))
-  anova_pipeline_cv = make_pipeline(var_filter_cv, anova_filter_cv, model)
-  anova_pipeline_cv.fit(X_train_cv, y_smote[train_idx])
-  y_pred_prob_cv = anova_pipeline_cv.predict_proba(X_test_cv)
-  y_pred_cv = anova_pipeline.predict(X_test_cv)
-  
-  
-  #auc_cv = roc_auc_score(y_smote[test_idx], y_pred_prob_cv.flatten(), multi_class='ovr')
-  y_pred_proba_cv_list.append(y_pred_prob_cv.flatten())
-  #auc_s_cv.append(auc_cv)
-  accuracy_s_cv.append(accuracy_score(y_smote[test_idx], y_pred_cv))
-  f1_s_cv.append(f1_score(y_smote[test_idx], y_pred_cv, average="weighted"))
-  scores_id_cv.append(test_idx)
-  scores_cv.append(y_pred_prob_cv)
-
-
-scores_id_cv = np.concatenate(scores_id_cv, axis=0) 
-scores_cv = np.concatenate(scores_cv, axis=0) 
-map_ids = []
-for i in range(len(sample_ids_cv)):
-  map_ids.append(np.where(scores_id_cv==i)[0][0])
-map_scores = scores_cv[map_ids]
-y_predict_cv = np.argmax(map_scores, axis=1)
-
-confusion_mat = confusion_matrix(y_, y_predict_cv)
-
-sns.set(style="white")
-labs=['Eraser','Writer', 'Reader']
-# Generate a large random dataset
-plt.figure(figsize=(4, 4))
-sns.set(font_scale=1.2)
-ax = sns.heatmap(confusion_mat, annot=True, square=True, xticklabels=labs, yticklabels=labs, cmap='RdPu', fmt="d",cbar_kws={"shrink": 0.85})
-ax.xaxis.tick_top()
-ax.yaxis.tick_left()
-ax.set_xticklabels(labs, fontsize=14)
-ax.set_yticklabels(labs, fontsize=14)
-plt.xlabel('Naive Bayes', fontsize=14, labelpad=11)
-plt.yticks(rotation=0) 
-plt.yticks(rotation=0) 
-
-plt.savefig("nb_loocv", format="png",bbox_inches='tight', dpi=600)
-
-accuracy_cv = np.mean(accuracy_s_cv)
-f1_cv = np.mean(f1_s_cv)
-auc_Score_cv = roc_auc_score(y_smote, y_pred_proba_cv_list, multi_class='ovr')
-predicted_prob_dict_cv[fold] = y_pred_prob_cv
-
-
-# In[ ]:
-
-
-print("Accuracy for Naive Bayes" , accuracy_cv)
-print("F1-score for Naive Bayes" , f1_cv)
-print("AUC score for Naive Bayes" , auc_Score_cv)
-
+    scores_id_cv = np.concatenate(scores_id_cv, axis=0) 
+    scores_cv = np.concatenate(scores_cv, axis=0) 
+    map_ids = []
+    for i in range(len(sample_ids_cv)):
+        map_ids.append(np.where(scores_id_cv==i)[0][0])
+    map_scores = scores_cv[map_ids]
+    y_predict_cv = np.argmax(map_scores, axis=1)
+    confusion_mat = confusion_matrix(y_, y_predict_cv)
+    sns.set(style="white")
+    labs=['Eraser','Writer', 'Reader']
+    # Generate a large random dataset
+    plt.figure(figsize=(4, 4))
+    sns.set(font_scale=1.2)
+    ax = sns.heatmap(confusion_mat, annot=True, square=True, xticklabels=labs, yticklabels=labs, cmap='RdPu', fmt="d",cbar_kws={"shrink": 0.85})
+    ax.xaxis.tick_top()
+    ax.yaxis.tick_left()
+    ax.set_xticklabels(labs, fontsize=14)
+    ax.set_yticklabels(labs, fontsize=14)
+    plt.xlabel('Naive Bayes', fontsize=14, labelpad=11)
+    plt.yticks(rotation=0) 
+    plt.yticks(rotation=0) 
+    plt.savefig("nb_loocv", format="png",bbox_inches='tight', dpi=600)
+    accuracy_cv = np.mean(accuracy_s_cv)
+    f1_cv = np.mean(f1_s_cv)
+    auc_Score_cv = roc_auc_score(y_smote, y_pred_proba_cv_list, multi_class='ovr')
+    predicted_prob_dict_cv[fold] = y_pred_prob_cv
+    print("Accuracy for Naive Bayes" , accuracy_cv)
+    print("F1-score for Naive Bayes" , f1_cv)
+    print("AUC score for Naive Bayes" , auc_Score_cv)
+    return [auc_Score_cv, accuracy_cv, f1_cv, confusion_mat]
 
 # In[ ]:
 
 
 #ANN Model
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import LeaveOneOut
+def deepNeuralNet(X_smote, y_smote, data):  
+    auc_s_loocv = []
+    accuracy_s_loocv = []
+    f1_s_loocv = []
+        #score_id
+    scores_id_loocv = []
+    scores_loocv = []
+    sample_ids_loocv = np.array(data.index)
+                
+    loocv = LeaveOneOut()
 
-    
-auc_s_loocv = []
-accuracy_s_loocv = []
-f1_s_loocv = []
-    
-    #score_id
-scores_id_loocv = []
-scores_loocv = []
-sample_ids_loocv = np.array(data.index)
-            
-loocv = LeaveOneOut()
-
-clf = MLPClassifier(hidden_layer_sizes = (100,100,), random_state=0, max_iter=5000)
-    
-for fold, (train_idx, test_idx) in enumerate(loocv.split(X_smote)):
-  X_train = X_smote[train_idx]
-  X_test = X_smote[test_idx]
-  anova_filter = SelectKBest(f_classif, k= 9)
-  var_filter = VarianceThreshold(threshold= .8 * (1 - .8))
-  anova_knn = make_pipeline(var_filter, anova_filter, clf)
-  anova_knn.fit(X_train, y_smote[train_idx])
-  y_pred_prob = anova_knn.predict_proba(X_test)
-  y_pred = anova_knn.predict(X_test)
-
-  auc_s_loocv.append(y_pred_prob.flatten())
-  accuracy_s_loocv.append(accuracy_score(y_smote[test_idx], y_pred))
-  f1_s_loocv.append(f1_score(y_smote[test_idx], y_pred, average='weighted'))      
-  scores_id_loocv.append(test_idx)
-  scores_loocv.append(y_pred_prob)
+    clf = MLPClassifier(hidden_layer_sizes = (100,100,), random_state=0, max_iter=5000)
         
-scores_id_loocv = np.concatenate(scores_id_loocv, axis=0) 
-scores_loocv = np.concatenate(scores_loocv, axis=0) 
-map_ids_loocv = []
-for i in range(len(sample_ids_loocv)):
-    map_ids_loocv.append(np.where(scores_id_loocv==i)[0][0])
-    map_scores_loocv = scores_loocv[map_ids_loocv]
-y_predict_loocv = np.argmax(map_scores_loocv, axis=1)
-    
-confusion_mat = confusion_matrix(y_, y_predict_loocv)
-sns.set(style="white")
-labs=['Eraser','Writer', 'Reader']
-    # Generate a large random dataset
-plt.figure(figsize=(4, 4))
-sns.set(font_scale=1.2)
-ax = sns.heatmap(confusion_mat, annot=True, square=True, xticklabels=labs, yticklabels=labs, cmap='RdPu', fmt="d",cbar_kws={"shrink": 0.85})
-ax.xaxis.tick_top()
-ax.yaxis.tick_left()
-ax.set_xticklabels(labs, fontsize=14)
-ax.set_yticklabels(labs, fontsize=14)
-plt.xlabel('Deep Neural Net', fontsize=14, labelpad=11)
+    for fold, (train_idx, test_idx) in enumerate(loocv.split(X_smote)):
+        X_train = X_smote[train_idx]
+        X_test = X_smote[test_idx]
+        anova_filter = SelectKBest(f_classif, k= 9)
+        var_filter = VarianceThreshold(threshold= .8 * (1 - .8))
+        anova_knn = make_pipeline(var_filter, anova_filter, clf)
+        anova_knn.fit(X_train, y_smote[train_idx])
+        y_pred_prob = anova_knn.predict_proba(X_test)
+        y_pred = anova_knn.predict(X_test)
 
-
-plt.yticks(rotation=0) 
-plt.xticks(rotation=0) 
-plt.savefig("dnn_loocv", format="png",bbox_inches='tight', dpi=600)
-plt.show()
+        auc_s_loocv.append(y_pred_prob.flatten())
+        accuracy_s_loocv.append(accuracy_score(y_smote[test_idx], y_pred))
+        f1_s_loocv.append(f1_score(y_smote[test_idx], y_pred, average='weighted'))      
+        scores_id_loocv.append(test_idx)
+        scores_loocv.append(y_pred_prob)
             
-#avg_score = np.mean(auc_s)
-accuracy_ann = np.mean(accuracy_s_loocv)
-f1_ann = np.mean(f1_s_loocv)
-auc_ann = roc_auc_score(y_smote, auc_s_loocv, multi_class='ovr')
-
-
-# In[ ]:
-
-
-print("Accuracy for ANN" , accuracy_ann)
-print("F1-score for ANN" , f1_ann)
-print("AUC score for ANN" ,  roc_auc_score(y_smote, auc_s_loocv, multi_class='ovr'))
-
+    scores_id_loocv = np.concatenate(scores_id_loocv, axis=0) 
+    scores_loocv = np.concatenate(scores_loocv, axis=0) 
+    map_ids_loocv = []
+    for i in range(len(sample_ids_loocv)):
+            map_ids_loocv.append(np.where(scores_id_loocv==i)[0][0])
+            map_scores_loocv = scores_loocv[map_ids_loocv]
+    y_predict_loocv = np.argmax(map_scores_loocv, axis=1)   
+    confusion_mat = confusion_matrix(y_, y_predict_loocv)
+    sns.set(style="white")
+    labs=['Eraser','Writer', 'Reader']
+        # Generate a large random dataset
+    plt.figure(figsize=(4, 4))
+    sns.set(font_scale=1.2)
+    ax = sns.heatmap(confusion_mat, annot=True, square=True, xticklabels=labs, yticklabels=labs, cmap='RdPu', fmt="d",cbar_kws={"shrink": 0.85})
+    ax.xaxis.tick_top()
+    ax.yaxis.tick_left()
+    ax.set_xticklabels(labs, fontsize=14)
+    ax.set_yticklabels(labs, fontsize=14)
+    plt.xlabel('Deep Neural Net', fontsize=14, labelpad=11)
+    plt.yticks(rotation=0) 
+    plt.xticks(rotation=0) 
+    plt.savefig("dnn_loocv", format="png",bbox_inches='tight', dpi=600)
+    plt.show()         
+    #avg_score = np.mean(auc_s)
+    accuracy_ann = np.mean(accuracy_s_loocv)
+    f1_ann = np.mean(f1_s_loocv)
+    auc_ann = roc_auc_score(y_smote, auc_s_loocv, multi_class='ovr')
+    print("Accuracy for ANN" , accuracy_ann)
+    print("F1-score for ANN" , f1_ann)
+    print("AUC score for ANN" ,  roc_auc_score(y_smote, auc_s_loocv, multi_class='ovr'))
+    return [auc_ann, accuracy_ann, f1_ann, confusion_mat]
 
 # In[14]:
 
 
 #KNN Model
-import numpy as np
-from sklearn import preprocessing
-from sklearn.metrics import mean_squared_error
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import KFold
-from sklearn.metrics import roc_auc_score
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.pipeline import make_pipeline
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_recall_curve
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-
 def knn_kfolds(X, y, n_neighbors, new_data, old_y, random_state=None):
     
     auc_s = []
@@ -307,7 +267,7 @@ def knn_kfolds(X, y, n_neighbors, new_data, old_y, random_state=None):
     
     for fold, (train_idx, test_idx) in enumerate(loocv.split(X,y)):
         
-        scaler = preprocessing.StandardScaler().fit(X)
+        scaler = StandardScaler().fit(X)
         X_train_scaled = scaler.transform(X[train_idx])
         X_test_scaled = scaler.transform(X[test_idx])
         
@@ -363,7 +323,7 @@ def knn_kfolds(X, y, n_neighbors, new_data, old_y, random_state=None):
 
 
 #Decision Tree Model
-from sklearn.tree import DecisionTreeClassifier
+
 
 def decision_tree(X, y, max_depth, new_data, old_y, random_state=None):
     
@@ -383,7 +343,7 @@ def decision_tree(X, y, max_depth, new_data, old_y, random_state=None):
     
     for fold, (train_idx, test_idx) in enumerate(loocv.split(X,y)):
         
-        scaler = preprocessing.StandardScaler().fit(X)
+        scaler = StandardScaler().fit(X)
         X_train_scaled = scaler.transform(X[train_idx])
         X_test_scaled = scaler.transform(X[test_idx])
         
@@ -441,7 +401,7 @@ def decision_tree(X, y, max_depth, new_data, old_y, random_state=None):
 
 
 #Random Forest Model
-from sklearn.ensemble import RandomForestClassifier
+
 
 def random_forest(X, y, max_depth, new_data, old_y, random_state=None):
     
@@ -461,7 +421,7 @@ def random_forest(X, y, max_depth, new_data, old_y, random_state=None):
     
     for fold, (train_idx, test_idx) in enumerate(loocv.split(X,y)):
         
-        scaler = preprocessing.StandardScaler().fit(X)
+        scaler = StandardScaler().fit(X)
         X_train_scaled = scaler.transform(X[train_idx])
         X_test_scaled = scaler.transform(X[test_idx])
         
@@ -522,9 +482,6 @@ def random_forest(X, y, max_depth, new_data, old_y, random_state=None):
 
 #SVM Model 
 
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
 
 def svm_f(X, y, new_data, old_y, random_state=None):
     
@@ -544,7 +501,7 @@ def svm_f(X, y, new_data, old_y, random_state=None):
     
     for fold, (train_idx, test_idx) in enumerate(loocv.split(X,y)):
         
-        scaler = preprocessing.StandardScaler().fit(X)
+        scaler = StandardScaler().fit(X)
         X_train_scaled = scaler.transform(X[train_idx])
         X_test_scaled = scaler.transform(X[test_idx])
         
@@ -601,8 +558,6 @@ def svm_f(X, y, new_data, old_y, random_state=None):
 
 
 #Feed-Forward Neural Network Model
-from sklearn.neural_network import MLPClassifier
-
 def fnn_f(X, y, new_data, old_y, random_state=None):
     
     auc_s = []
@@ -621,7 +576,7 @@ def fnn_f(X, y, new_data, old_y, random_state=None):
     
     for fold, (train_idx, test_idx) in enumerate(loocv.split(X,y)):
         
-        scaler = preprocessing.StandardScaler().fit(X)
+        scaler = StandardScaler().fit(X)
         X_train_scaled = scaler.transform(X[train_idx])
         X_test_scaled = scaler.transform(X[test_idx])
         
@@ -678,8 +633,6 @@ def fnn_f(X, y, new_data, old_y, random_state=None):
 
 
 #Logistic Regression Model
-from sklearn.linear_model import LogisticRegression
-
 def lr(X, y, new_data, old_y, random_state=None):
     
     auc_s = []
@@ -698,7 +651,7 @@ def lr(X, y, new_data, old_y, random_state=None):
     
     for fold, (train_idx, test_idx) in enumerate(loocv.split(X,y)):
         
-        scaler = preprocessing.StandardScaler().fit(X)
+        scaler = StandardScaler().fit(X)
         X_train_scaled = scaler.transform(X[train_idx])
         X_test_scaled = scaler.transform(X[test_idx])
         
@@ -758,10 +711,6 @@ def lr(X, y, new_data, old_y, random_state=None):
 
 
 #Running the ML models on the generated dataset
-
-from sklearn.metrics import plot_confusion_matrix
-from sklearn.metrics import confusion_matrix
-
 results = {}
 auc = []
 accuracy = []
@@ -785,6 +734,8 @@ for i in k:
 svm = svm_f(X_smote, y_smote, data, y_, random_state=5)
 fnn = fnn_f(X_smote, y_smote, data, y_, random_state=5)
 lr_s = lr(X_smote, y_smote, data, y_, random_state=5)
+nb = naiveBayes(X_smote, y_smote, data)
+dnn = deepNeuralNet(X_smote, y_smote, data)
 
 auc.append(max(knn_values))
 auc.append(max(rf_values))
@@ -792,6 +743,8 @@ auc.append(max(dt_values))
 auc.append(fnn[0])
 auc.append(lr_s[0])
 auc.append(svm[0])
+auc.append(nb[0])
+auc.append(dnn[0])
 
 accuracy.append(knn[1])
 accuracy.append(dt[1])
@@ -799,6 +752,8 @@ accuracy.append(rf[1])
 accuracy.append(fnn[1])
 accuracy.append(lr_s[1])
 accuracy.append(svm[1])
+accuracy.append(nb[1])
+accuracy.append(dnn[1])
 
 f1.append(knn[2])
 f1.append(dt[2])
@@ -806,7 +761,8 @@ f1.append(rf[2])
 f1.append(fnn[2])
 f1.append(lr_s[2])
 f1.append(svm[2])
-
+f1.append(nb[2])
+f1.append(dnn[2])
 
 confusion_matrices.append(knn[3])
 confusion_matrices.append(dt[3])
@@ -814,6 +770,8 @@ confusion_matrices.append(rf[3])
 confusion_matrices.append(fnn[3])
 confusion_matrices.append(lr_s[3])
 confusion_matrices.append(svm[3])
+confusion_matrices.append(nb[3])
+confusion_matrices.append(dnn[3])
 
 results['auc'] = auc
 #results['accuracy'] = accuracy
@@ -831,7 +789,7 @@ results['confusion_matrix'] = confusion_matrices
 
 sns.set(style="white")
 labs=['Eraser','Writer', 'Reader']
-models = ["K-Neighbors Classifier", "Decision Tree", "Random Forest", "Feed-Forward Neural Network", "Logistic Regression","Support Vector Machine"]
+models = ["K-Neighbors Classifier", "Decision Tree", "Random Forest", "Feed-Forward Neural Network", "Logistic Regression","Support Vector Machine", "Naive Bayes", "Deep Neural Network"]
 i = 0
 img_file ="loocv"
 for m in results['confusion_matrix']:
